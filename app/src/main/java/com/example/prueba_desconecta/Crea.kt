fc5ec325f633af4502019d7067f7e8fcdb9738b8
arroy97 <62266330+arroy97@users.noreply.github.com>
2020-11-19 14:29:12 +0100

    hector_1.1

--- app/src/main/java/com/example/prueba_desconecta/Crea.kt
+++ app/src/main/java/com/example/prueba_desconecta/Crea.kt
@@ -1,19 +1,56 @@
 package com.example.prueba_desconecta
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_crea.*
+import org.json.JSONObject
+
 
 class Crea : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crea)
+        val resp : TextView = findViewById(R.id.httpResp)
 
         val btn: Button = findViewById(R.id.buttonCreate)
         btn.setOnClickListener{
-            val r = Intent(this, Revisita::class.java)
-            startActivity(r)
+            //val r = Intent(this, Revisita::class.java)
+            //startActivity(r)
+            val queue = Volley.newRequestQueue(this)
+            //val url = "http://pae-ics.etsetb.upc.edu/api"
+            val url = httpProve.text.toString()
+
+            val jsonArrayReq = JsonArrayRequest(url,Response.Listener {
+                    response ->
+                       resp.setText("Response is: ".format(response.toString()))
+                     },
+                    Response.ErrorListener { error->
+                      resp.setText(error.message)
+                    })
+        // Request a string response from the provided URL.
+            //val jsonRequest = JsonObjectRequest(Request.Method.GET, url, null,  Response.Listener {
+            //    response ->
+            //       resp.setText("Response is: ".format(response.toString()))
+           //     },
+            //    Response.ErrorListener { error->
+            //      resp.setText(error.message)
+            //    })
+
+            queue.add(jsonArrayReq)
+
+
+            // Access the RequestQueue through your singleton class.
+            //https://developer.android.com/training/volley/requestqueue
+            //MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
         }
+
     }
 }
