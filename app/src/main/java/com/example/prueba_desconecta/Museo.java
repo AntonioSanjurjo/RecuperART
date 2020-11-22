2ae2f5ca45ec3c9e8ec891886b435848e0ccd924
andreastourino <andreas.tourinoo@gmail.com>
2020-11-22 13:55:56 +0100

    Andreas 1.2

---
+++ app/src/main/java/com/example/prueba_desconecta/Museo.java
@@ -0,0 +1,31 @@
+package com.example.prueba_desconecta;
+
+import java.util.ArrayList;
+
+import javax.xml.namespace.QName;
+
+public class Museo {
+
+    private String nombre
+    private ArrayList<Obra> obras
+
+
+    public Museo(String name, ArrayList<Obra> work ){
+        nombre = name
+        obras = work
+    }
+
+    public ArrayList<Obra> getobralist(){
+        return obras;
+
+    }
+
+    public Obra getObra(int i){
+        return Obra[i]
+    }
+
+    public String getNombre(){
+        return nombre
+    }
+
+}
