8c16c857d62555fcc181d974dd140eba136437c5
andreastourino <andreas.tourinoo@gmail.com>
2020-12-03 11:07:30 +0100

    Andreas 1.2.3

--- app/src/main/java/com/example/prueba_desconecta/ui/Museo.java
+++ app/src/main/java/com/example/prueba_desconecta/ui/Museo.java
@@ -6,24 +6,61 @@ public class Museo {
 
     private String nombre;
     private ArrayList<Obra> obras;
+    private String id;
+    private String tipo;
+    private String direccion;
 
 
-    public Museo(String name, ArrayList<Obra> work ){
-        nombre = name;
-        obras = work;
+    public Museo(){
+
     }
 
     public ArrayList<Obra> getobralist(){
-        return obras;
+        return getObras();
 
     }
 
     public Obra getObra(int i){
-        return obras.get(i);
+        return getObras().get(i);
     }
 
     public String getNombre(){
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Obra> getObras() {
+        return obras;
+    }
+
+    public void setObras(ArrayList<Obra> obras) {
+        this.obras = obras;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
--- app/src/main/java/com/example/prueba_desconecta/ui/Obra.java
+++ app/src/main/java/com/example/prueba_desconecta/ui/Obra.java
@@ -3,21 +3,55 @@ package com.example.prueba_desconecta.ui;
 public class Obra {
 
     
-private String nombre;
+
 private String tipo;
+private String id;
+private String descripcion;
+private String autor;
+private Museo museo;
 
-public Obra(String name, String type){
-    nombre = name;
-    tipo = type;
-}
+public Obra(){
 
-public String getName(){
-    return nombre;
-}
-
-public String getType(){
-    return tipo;
 }
 
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Museo getMuseo() {
+        return museo;
+    }
+
+    public void setMuseo(Museo museo) {
+        this.museo = museo;
+    }
 }
