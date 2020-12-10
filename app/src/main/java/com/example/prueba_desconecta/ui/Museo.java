f35326ec31b384fbd2c5f790ff929e5ade44c4cf
andreastourino <andreas.tourinoo@gmail.com>
2020-12-10 13:10:09 +0100

    Andreas 1.2.5

--- app/src/main/java/com/example/prueba_desconecta/ui/Museo.java
+++ app/src/main/java/com/example/prueba_desconecta/ui/Museo.java
@@ -5,24 +5,25 @@ import java.util.ArrayList;
 public class Museo {
 
     private String nombre;
-    private ArrayList<Obra> obras;
+    //private ArrayList<Obra> obras;
     private String id;
     private String tipo;
     private String direccion;
+    private String descripcion;
 
 
     public Museo(){
 
     }
 
-    public ArrayList<Obra> getobralist(){
-        return getObras();
+   // public ArrayList<Obra> getobralist(){
+     //   return getObras();
 
-    }
+    //}
 
-    public Obra getObra(int i){
-        return getObras().get(i);
-    }
+   // public Obra getObra(int i){
+     //   return getObras().get(i);
+  //  }
 
     public String getNombre(){
         return nombre;
@@ -32,13 +33,13 @@ public class Museo {
         this.nombre = nombre;
     }
 
-    public ArrayList<Obra> getObras() {
-        return obras;
-    }
+    //public ArrayList<Obra> getObras() {
+   //     return obras;
+   // }
 
-    public void setObras(ArrayList<Obra> obras) {
-        this.obras = obras;
-    }
+   // public void setObras(ArrayList<Obra> obras) {
+    //    this.obras = obras;
+   // }
 
     public String getId() {
         return id;
@@ -62,5 +63,13 @@ public class Museo {
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
